bcbc6edbb6cd041d627e66e9662ead22903bacb5
timImuno <tim@imunotx.com>
2023-12-09 18:17:25 +0100

    p2

---
+++ day9/day9p2.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+from collections import deque
+
+def parseInput(content):
+    return [list(map(int,line.split())) for line in content.splitlines()]
+    
+    
+def buildNumberTree(numberList):
+    numberTree = deque([deque(numberList)])
+    while numberTree[-1] != deque([0]*len(numberTree[-1])):
+        nextNumReeks = deque([])
+        lastNums = numberTree[-1]
+        for i in range(0,len(lastNums)-1):
+            nextNumReeks.append(lastNums[i]-lastNums[i+1])
+        numberTree.append(nextNumReeks)
+    return numberTree
+    
+def predictNextNum(numberTree):
+    addToNextCycle = 0
+    for nums in reversed(numberTree):
+        nums.appendleft(nums[0]+addToNextCycle)
+        addToNextCycle = nums[0]
+    return numberTree[0][0]
+
+
+
+def main():
+    with open("day9/input.txt") as file:
+        numbers = parseInput(file.read().strip())
+
+    nextNumList = []
+    for nextNums  in numbers:
+        numberTree = buildNumberTree(nextNums)
+        nextNumList.append(predictNextNum(numberTree))
+
+    print(sum(nextNumList))
+     
+        
+if __name__ == '__main__':
+    import time
+    start_time = time.time()
+    main()
+    print('--- %s seconds ---' % (time.time() - start_time))
